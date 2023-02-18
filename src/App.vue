a28e64c8e85feb0b5dd4e4400953b535ec67e06c
Kasem32833 <abulkasem32833@gmail.com>
2023-02-18 09:14:23 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+
+<template>
+      <!-- Hero Area -->
+      <div class="relative left-0 top-0 bg-gradient-to-r from-purple-500 to-orange-400 custom-shape flex flex-col space-y-6 justify-center items-center text-white position-top ">
+        <img src="assets/img/profile.jpeg" alt="">
+        <p>Hello</p>
+        <h1 class="text-7xl">I'm <span class="auto-type text-black">Fronend Developer</span></h1>
+    </div>
+    <!-- Navigation area -->
+    <nav class="absolute left-0 top-0 w-full  bg-transparent">
+        <div class="container mx-auto">
+            <div class="flex items-center justify-between p-4">
+                <div class="logo-area text-white">
+                    <h3 class="text-2xl">Kase<span class="text-red-500">m</span></h3>
+                </div>
+                <div class="menu-area text-white space-x-3">
+                    <a href="#">Home</a>
+                    <a href="#">About Me</a>
+                    <a href="#">Services</a>
+                    <a href="#">Pricing</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+
+</style>
